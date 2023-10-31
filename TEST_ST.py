8fdae0f0555141874a28a834c25e28f382ba15af
hampus.londogard@gmail.com <hampus.londogard@gmail.com>
2023-10-31 16:26:40 +0100

    wip

---
+++ TEST_ST.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+def main():
+    st.header("Hello World!")
+    # solara.selectbox("Select Twitch ID") show-case re-run slow and caching
+
+
+if __name__ == "__main__":
+    main()
